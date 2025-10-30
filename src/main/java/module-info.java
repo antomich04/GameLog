658cbef3fcb7d936eb767756dc85415c86a7eda1
Michael Papageorgiou <micelpap4@gmail.com>
2025-10-30 20:26:50 +0200

    implement Sign_Up Page

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.gamelog.gamelog {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.gamelog.gamelog to javafx.fxml;
--- src/main/java/org/gamelog/gamelog/StartingPageController.java
+++ src/main/java/org/gamelog/gamelog/StartingPageController.java
@@ -1,4 +1,31 @@
 package org.gamelog.gamelog;
 
-public class StartingPageController{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartingPageController {
+
+    @FXML
+    private Button getStartedButton;
+
+    @FXML
+    private void handleGetStarted(ActionEvent event) {
+        try {
+            Parent signUpRoot = FXMLLoader.load(getClass().getResource("sign_up_page.fxml"));
+            Scene signUpScene = new Scene(signUpRoot);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(signUpScene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
